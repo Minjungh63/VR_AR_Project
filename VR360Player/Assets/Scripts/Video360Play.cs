5ddb3514f8af2aa6904e745c42bcd8aa0b228584
MinJung <52921222+Minjungh63@users.noreply.github.com>
2022-07-15 00:32:50 +0900

    360 영상 플레이어 제작

---
+++ VR360Player/Assets/Scripts/Video360Play.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.Video; // VideoPlayer 기능을 이용하기 위한 네임스페이스
+// 비디오 플레이어를 통해 360 스피어에 영상을 재생
+// 두 가지 서로 다른 영상을 교체하며 재생
+
+public class Video360Play : MonoBehaviour
+{
+    VideoPlayer vp; // 비디오 플레이어 컴포넌트
+    public VideoClip[] vcList; // 다수의 비디오 클립을 배열로 만들어 관리
+    int curVCidx; // 현재 재생중인 클립 번호를 저장
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vp = GetComponent<VideoPlayer>();
+        vp.clip = vcList[0];
+        curVCidx=0;
+        vp.Stop();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftBracket)){ // 왼쪽 대괄호 입력시
+            SwapVideoClip(false);
+        }
+        else if(Input.GetKeyDown(KeyCode.RightBracket)){ // 오른쪽 대괄호 입력시
+            SwapVideoClip(true);
+        }
+    }
+    public void SwapVideoClip(bool isNext){
+        int setVCnum = curVCidx;
+        vp.Stop();
+        if(isNext){
+            setVCnum = (setVCnum+1)%vcList.Length;
+        }
+        else{
+            setVCnum=((setVCnum-1)+vcList.Length)%vcList.Length;
+        }
+        vp.clip=vcList[setVCnum];
+        vp.Play();
+        curVCidx = setVCnum;
+    }
+    public void SetVideoPlay(int num){
+        if(curVCidx != num){
+            vp.Stop();
+            vp.clip = vcList[num];
+            curVCidx = num;
+            vp.Play();
+        }
+    }
+}
+
---
+++ VR360Player/Assets/Scripts/VideoFrame.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Video; // VideoPlayer 기능을 사용하기 위한 네임스페이스
+// VideoPlayer 컴포넌트 제어 코드
+public class VideoFrame : MonoBehaviour{
+    // VideoPlayer 컴포넌트
+    VideoPlayer vp;
+
+    void Start(){
+        // 현재 오브젝트의 비디오 플레이어 컴포넌트 정보를 가지고 온다.
+        vp = GetComponent<VideoPlayer>();
+        // 자동 재생되는 것을 막는다.
+        vp.Stop();
+    }
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.S)){
+            vp.Stop();
+        }
+        if(Input.GetKeyDown("space")){
+            if(vp.isPlaying){
+                vp.Pause();
+            }else{
+                vp.Play();
+            }
+        }
+    }
+    public void CheckVideoFrame(bool Checker){
+        if(Checker){
+            if(!vp.isPlaying){
+                vp.Play();
+            }
+        }else{
+            vp.Stop();
+        }
+    }
+}
